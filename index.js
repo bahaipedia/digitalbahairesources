0536d263dac03dc38b022fdcaa2f50a948a76eaa
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2024-12-11 11:28:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Routes
 app.get('/', (req, res) => res.render('index'));
 app.get('/about', (req, res) => res.render('about'));
+app.get('/projects', (req, res) => res.render('projects'));
 app.get('/contact', (req, res) => res.render('contact'));
 
 app.listen(PORT, () => {
