77d6a02bec7ef26369f7f4982c5819ac60f6e3e5
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2024-12-12 04:03:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2/promise');
+const winston = require('winston');
 require('dotenv').config();
 
 const app = express();
