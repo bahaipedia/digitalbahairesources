808b83761a5f70146c33f14e7e9aaab6a54da2e5
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2025-07-13 10:33:13 -0700

    Update pageview-analysis.js

--- public/pageview-analysis.js
+++ public/pageview-analysis.js
@@ -217,6 +217,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 format: 'json',
                 titles: title,
                 prop: 'info|revisions',
+                inprop: 'length',
                 rvprop: 'user',
                 rvlimit: 'max',
                 origin: '*'
